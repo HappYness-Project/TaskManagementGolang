9f50c1541690cff078b543a64ca7eae589c9fd71
kevin <hyunbin7303@gmail.com>
2024-08-06 23:18:44 -0400

    sample route unit testing

---
+++ internal/taskcontainer/routes_test.go
@@ -0,0 +1,57 @@
+package taskcontainer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func TestTaskContainerHandler(t *testing.T) {
+	repo := &mockContainerRepo{}
+	handler := NewHandler(repo)
+
+	t.Run("when get all task containers, Then return status code 200", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, "/api/task-containers", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		mux := chi.NewRouter()
+
+		mux.Get("/api/task-containers", handler.handleGetTaskContainers)
+
+		mux.ServeHTTP(rr, req)
+		if rr.Code != http.StatusOK {
+			t.Errorf("expected status code %d, got %d", http.StatusOK, rr.Code)
+		}
+	})
+
+	t.Run("when get task container by containerId, Then return status code 200", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, "/api/task-containers/abcd", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		mux := chi.NewRouter()
+
+		mux.Get("/api/task-containers/{containerID}", handler.handleGetTaskContainerById)
+
+		mux.ServeHTTP(rr, req)
+		if rr.Code != http.StatusOK {
+			t.Errorf("expected status code %d, got %d", http.StatusOK, rr.Code)
+		}
+	})
+}
+
+type mockContainerRepo struct{}
+
+func (m *mockContainerRepo) AllTaskContainers() ([]*TaskContainer, error) {
+	return []*TaskContainer{}, nil
+}
+func (m *mockContainerRepo) GetById(id string) (*TaskContainer, error) {
+	return &TaskContainer{}, nil
+}
