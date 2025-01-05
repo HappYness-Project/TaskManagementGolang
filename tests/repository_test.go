5312e7266fc19b1ca9252178babebc6ed8c9856d
kevin <hyunbin7303@gmail.com>
2025-01-04 23:18:02 -0500

    integration test - but timeout scenario is not working

--- tests/repository_test.go
+++ tests/repository_test.go
@@ -1,101 +1,68 @@
 package tests
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
-	"log"
 	"testing"
 	"time"
 
-	"github.com/google/uuid"
-	_ "github.com/lib/pq"
-
-	"github.com/happYness-Project/taskManagementGolang/internal/task/model"
 	"github.com/happYness-Project/taskManagementGolang/internal/task/repository"
-	"github.com/ory/dockertest/v3"
-	"github.com/ory/dockertest/v3/docker"
+	"github.com/happYness-Project/taskManagementGolang/pkg/configs"
+	"github.com/happYness-Project/taskManagementGolang/pkg/dbs"
+	"github.com/happYness-Project/taskManagementGolang/pkg/loggers"
 	"github.com/stretchr/testify/require"
 )
 
 var db *sql.DB
 
+const dbTimeout = time.Second * 5
+
 func TestMain(m *testing.M) {
-	// uses a sensible default on windows (tcp/http) and linux/osx (socket)
-	pool, err := dockertest.NewPool("")
-	if err != nil {
-		log.Fatalf("Could not construct pool: %s", err)
-	}
+	env := configs.InitConfig("")
+	configs.AccessToken = env.AccessTokenSecret
+	var connStr = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable timezone=UTC connect_timeout=5",
+		env.DBHost, env.DBPort, env.DBUser, env.DBPwd, env.DBName)
+	logger := loggers.Setup(env)
+	logger.Info().Msg(connStr)
+	db, _ = dbs.ConnectToDb(connStr)
 
-	err = pool.Client.Ping()
-	if err != nil {
-		log.Fatalf("Could not connect to Docker: %s", err)
-	}
-
-	// pulls an image, creates a container based on it and runs it
-	resource, err := pool.RunWithOptions(&dockertest.RunOptions{
-		Repository: "postgres",
-		Tag:        "11",
-		Env: []string{
-			"POSTGRES_PASSWORD=secret",
-			"POSTGRES_USER=user_name",
-			"POSTGRES_DB=dbname",
-			"listen_addresses = '*'",
-		},
-	}, func(config *docker.HostConfig) {
-		// set AutoRemove to true so that stopped container goes away by itself
-		config.AutoRemove = true
-		config.RestartPolicy = docker.RestartPolicy{Name: "no"}
-	})
-	if err != nil {
-		log.Fatalf("Could not start resource: %s", err)
-	}
-
-	hostAndPort := resource.GetHostPort("5432/tcp")
-	databaseUrl := fmt.Sprintf("postgres://user_name:secret@%s/dbname?sslmode=disable", hostAndPort)
-
-	log.Println("Connecting to database on url: ", databaseUrl)
-
-	resource.Expire(120) // Tell docker to hard kill the container in 120 seconds
-
-	// exponential backoff-retry, because the application in the container might not be ready to accept connections yet
-	pool.MaxWait = 120 * time.Second
-	if err = pool.Retry(func() error {
-		db, err = sql.Open("postgres", databaseUrl)
-		if err != nil {
-			return err
-		}
-		return db.Ping()
-	}); err != nil {
-		log.Fatalf("Could not connect to docker: %s", err)
-	}
-
-	defer func() {
-		if err := pool.Purge(resource); err != nil {
-			log.Fatalf("Could not purge resource: %s", err)
-		}
-	}()
-
-	// run tests
 	m.Run()
 }
 
-func Test_GetAllTasks_ReturnNothing(t *testing.T) {
+func Test_TaskRepository_GetAllTasks_ReturnSuccess(t *testing.T) {
 	taskRepo := repository.NewTaskRepository(db)
 
-	tasks, _ := taskRepo.GetAllTasks()
+	tasks, err := taskRepo.GetAllTasks()
 
-	require.Nil(t, tasks)
+	require.Nil(t, err)
+	require.NotNil(t, tasks)
 }
 
-func Test_GetAllTasks_When_Tasks_Exist_Return_Tasks(t *testing.T) {
+func Test_TaskRepository_GetAllTasks_Timeout(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*7)
+	defer cancel()
+
+	// Simulate a long-running query in the database
+	_, err := db.Exec("SELECT pg_sleep(2)") // Simulate 2-second delay
+	require.Nil(t, err)
+
 	taskRepo := repository.NewTaskRepository(db)
 
-	taskRepo.CreateTask(uuid.NewString(), model.Task{
-		TaskId:   uuid.NewString(),
-		TaskDesc: "",
-	})
-
-	tasks, _ := taskRepo.GetAllTasks()
-
+	tasks, err := taskRepo.GetAllTasks()
 	require.Nil(t, tasks)
+
+	select {
+
+	case <-ctx.Done():
+		t.Error("timeout")
+	}
+	select {
+	// case output := <-uut.Output:
+	// 	if output != 25 {
+	// 		t.Errorf("expected 25 got %d", output)
+	// 	}
+	case <-ctx.Done():
+		t.Error("timeout")
+	}
 }
