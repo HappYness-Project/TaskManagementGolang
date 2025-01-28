b7c390ca4fc82f91baca1b59d995e221d813b3be
kevin <hyunbin7303@gmail.com>
2025-01-27 22:16:00 -0500

    Missing default_group_id from the sql queries

--- internal/user/repository/user_sql_queries.go
+++ internal/user/repository/user_sql_queries.go
@@ -2,16 +2,16 @@ package repository
 
 const (
 	sqlGetAllUsers = `SELECT * FROM public.user`
-	sqlGetUserById = `SELECT id, username, first_name, last_name, email, is_active,created_at,updated_at, usersetting_id
+	sqlGetUserById = `SELECT id, username, first_name, last_name, email, is_active,created_at,updated_at, default_group_id
 						 FROM public.user
 						 WHERE id = $1`
-	sqlGetUserByEmail = `SELECT id, username, first_name, last_name, email, is_active,created_at,updated_at, usersetting_id
+	sqlGetUserByEmail = `SELECT id, username, first_name, last_name, email, is_active,created_at,updated_at, default_group_id
 							FROM public.user
 							WHERE email = $1`
-	sqlGetUserByUsername = `SELECT id, username, first_name, last_name, email, is_active,created_at,updated_at, usersetting_id
+	sqlGetUserByUsername = `SELECT id, username, first_name, last_name, email, is_active,created_at,updated_at, default_group_id
 							FROM public.user
 							WHERE username = $1`
-	sqlGetUsersByGroupId = `SELECT id, username, first_name, last_name, email, is_active,created_at,updated_at, usersetting_id from public.user u
+	sqlGetUsersByGroupId = `SELECT id, username, first_name, last_name, email, is_active,created_at,updated_at, default_group_id from public.user u
 							INNER JOIN public.usergroup_user ugu
 							ON u.id = ugu.user_id
 							WHERE ugu.usergroup_id = $1`
