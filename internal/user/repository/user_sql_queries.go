30d75ef46132742ebca6622558dc5dce7b9e1ed9
kevin <hyunbin7303@gmail.com>
2025-03-16 00:18:13 -0400

    quick fixes for the sqlGetUsers by Group Id query.

--- internal/user/repository/user_sql_queries.go
+++ internal/user/repository/user_sql_queries.go
@@ -11,7 +11,7 @@ const (
 	sqlGetUserByUsername = `SELECT id, user_id, username, first_name, last_name, email, is_active,created_at,updated_at, default_group_id
 							FROM public.user
 							WHERE username = $1`
-	sqlGetUsersByGroupId = `SELECT id, user_id, username, first_name, last_name, email, is_active,created_at,updated_at, default_group_id from public.user u
+	sqlGetUsersByGroupId = `SELECT id, u.user_id, username, first_name, last_name, email, is_active,created_at,updated_at, default_group_id from public.user u
 							INNER JOIN public.usergroup_user ugu
 							ON u.id = ugu.user_id
 							WHERE ugu.usergroup_id = $1`
